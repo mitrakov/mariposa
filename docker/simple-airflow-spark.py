5ddcc88adcad7ac7bfa2f1a8a8d98cd0324c92f9
tommy-air <mitrakov-artem@yandex.ru>
2026-03-22 13:54:35 +0300

    Airflow works

---
+++ docker/simple-airflow-spark.py
@@ -0,0 +1,35 @@
+# first create a connection spark_default with host 'yarn'!
+import os
+import glob
+from airflow import DAG
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from datetime import datetime
+
+# Helper to find the examples JAR dynamically
+SPARK_HOME = os.getenv('SPARK_HOME', '/opt/spark')
+JAR_PATTERN = f"{SPARK_HOME}/examples/jars/spark-examples_*.jar"
+found_jars = glob.glob(JAR_PATTERN)
+EXAMPLES_JAR = found_jars[0] if found_jars else "NOT_FOUND"
+
+with DAG(
+    dag_id='spark_yarn_connection_test',
+    start_date=datetime(2026, 3, 22),
+    schedule=None,
+    catchup=False,
+    tags=['testing', 'spark'],
+) as dag:
+
+    submit_job = SparkSubmitOperator(
+        task_id='submit_spark_pi',
+        application=EXAMPLES_JAR,
+        java_class='org.apache.spark.examples.SparkPi',
+        conn_id='spark_default',
+        application_args=['10'], # Increased to 10 so it stays in YARN UI longer
+        conf={
+            "spark.master": "yarn",
+            "spark.submit.deployMode": "client",
+            "spark.executor.memory": "512m",
+            "spark.driver.memory": "512m"
+        },
+        name='airflow-spark-test-pi'
+    )
