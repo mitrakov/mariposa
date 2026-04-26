2753ea136aa81c943598145efaaf8467dc65d724
tommy-air <mitrakov-artem@yandex.ru>
2026-04-26 17:47:31 +0300

    Small fix

--- mariposa-framework/src/main/scala/com/mitrakoff/mariposa/Hive2Kafka.scala
+++ mariposa-framework/src/main/scala/com/mitrakoff/mariposa/Hive2Kafka.scala
@@ -1,10 +1,11 @@
 package com.mitrakoff.mariposa
 
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, struct, to_json}
 import org.slf4j.LoggerFactory
 
-case class Hive2Kafka private (
+
+case class Hive2Kafka  private (
     private val hiveTable: String = "myTable",
     private val kafkaTopic: String = "myTopic",
     private val kafkaBootstrapServers: String = "localhost:9092",
@@ -12,8 +13,8 @@ case class Hive2Kafka private (
   private val logger = LoggerFactory.getLogger(getClass)
 
   def withHiveTable(table: String): Hive2Kafka = copy(hiveTable = table)
-  def withKafkaTopic(topic: String): Hive2Kafka = copy(kafkaTopic = topic)
-  def withKafkaBootstrapServers(servers: String): Hive2Kafka = copy(kafkaBootstrapServers = servers)
+  def withKafkaTopic(topic: String): Hive2Kafka  = copy(kafkaTopic = topic)
+  def withKafkaBootstrapServers(servers: String): Hive2Kafka  = copy(kafkaBootstrapServers = servers)
 
   def build(): Runnable = () => {
     logger.info("=== Mariposa-Hive2Kafka ===")
@@ -24,22 +25,22 @@ case class Hive2Kafka private (
       .getOrCreate()
 
     val hiveDF = spark.table(hiveTable)
+
+
+
     logger.info(s"Reading from Hive table: $hiveTable and publishing to Kafka: $kafkaTopic")
 
-    // Converting every row into a JSON for Kafka
-    val kafkaPayloadDF = hiveDF.select(to_json(struct(allColumns(hiveDF): _*)).as("value"))
-
-    kafkaPayloadDF.write
+    // Transform to a JSON and send to Kafka
+    val toKafkaDF = hiveDF.select(to_json(struct(hiveDF.columns.map(col): _*)).as("value"))
+    toKafkaDF.write
       .format("kafka")
       .option("kafka.bootstrap.servers", kafkaBootstrapServers)
       .option("topic", kafkaTopic)
       .save()
 
-    logger.info("Hive to Kafka completed successfully.")
+    logger.info("Hive  to Kafka completed successfully.")
     spark.stop()
   }
-
-  private def allColumns(df: DataFrame) = df.columns.map(col)
 }
 
 object Hive2Kafka {
