0b7c26a6d40809037eb537dd2649984e1991a728
tommy <mitrakov-artem@yandex.ru>
2026-06-28 14:40:15 +0300

    Add 2 more crawlers for zakupki

---
+++ crawlers/zakupki/fz223/spark-zakupki-import.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+JKS_PASSWORD=...
+spark-submit \
+  --deploy-mode cluster \
+  --driver-java-options=" \
+   -Dapp.hive.table=zakupki.fz223_import \
+   -Dapp.kafka.topic=zakupki-fz223-import \
+   -Dapp.kafka.run.infinitely=true \
+   -Dapp.security.truststore.password=$JKS_PASSWORD \
+   -Djava.security.auth.login.config=$KAFKA_HOME/config/kafka_jaas.conf" \
+  --conf "spark.executor.extraJavaOptions=-Djava.security.auth.login.config=$KAFKA_HOME/config/kafka_jaas.conf" \
+  --class com.mitrakoff.mariposa.Kafka2Hive \
+  mariposa-assembly-1.0.0.jar &
