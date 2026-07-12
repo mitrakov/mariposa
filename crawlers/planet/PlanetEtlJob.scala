8e6e464c34a1524c9bd910412910c4dc4cb5c007
tommy <mitrakov-artem@yandex.ru>
2026-07-12 17:38:26 +0300

    add male names

--- crawlers/planet/PlanetEtlJob.scala
+++ crawlers/planet/PlanetEtlJob.scala
@@ -59,7 +59,7 @@ class PlanetEtlJob {
       "юля", "юлия", "наталья", "таисия", "мия", "галина", "ира", "людмила", "вика", "олеся",
       "евгения", "ника", "алия", "элoна", "карина", "эльмира", "тата", "инна", "тартуга", "натали",
       "василиса", "наташа", "леся", "рита", "валентина", "нина", "алиса", "наина", "дарья", "любовь",
-      "таня", "зоя"
+      "таня", "зоя", "соня", "элизабет", "лилия", "гузель", "айгуль", "мари"
     )
     val maleNames: Set[String] = Set(
       "андрей", "игорь", "евгений", "руслан", "азим", "марк", "владимир", "максим", "виталий",
@@ -76,7 +76,15 @@ class PlanetEtlJob {
       "ислам", "самандар", "нодир", "шавкат", "петр", "валентин", "рашид", "пётр", "арсентий",
       "артемий", "леша", "захар", "нурик", "аман", "маруф", "леонид", "наваи", "армен", "энвер",
       "мердан", "арман", "амир", "слава", "гена", "бурхон", "юра", "равиль", "рамин", "seraфим",
-      "гриня", "ахмед", "аманулла", "азиз", "даниил", "василий"
+      "гриня", "ахмед", "аманулла", "азиз", "даниил", "василий", "ренат", "ахмат", "едуард", "макс",
+      "михо", "ахмет", "пабло", "педро", "мурад", "ринат", "мухаммет", "ричард", "мехмет", "франческо",
+      "гари", "тони", "мохамед", "шах", "андреи", "алекс", "салах", "марсель", "махмад", "антони",
+      "омар", "халид", "фатих", "сократ", "роберто", "магамед", "алмат", "камиль", "альфред",
+      "роберт", "мурат", "рамиль", "зухроб", "марат", "аират", "гриша", "шухрат", "джони", "юсуф",
+      "даниэль", "фернандо", "август", "амед", "серж", "бехруз", "армат", "аркадий", "энджи", "йосе",
+      "мухаммад", "дмитри", "азамат", "лео", "игнат", "аледжандро", "джорге", "шамиль", "сергей",
+      "сергеи", "мике", "филипп", "эд", "сергио", "шерзод", "эмиль", "франциско", "антонио", "магомед",
+      "арт", "рикардо", "андрев"
     )
     val maleStatuses   = Seq("Свободен", "Женат", "Разведен")
     val femaleStatuses = Seq("Свободна", "Замужем", "Разведена")
@@ -84,9 +92,11 @@ class PlanetEtlJob {
       "shch" -> "щ", "sh" -> "ш", "ch" -> "ч", "zh" -> "ж", "kh" -> "х",
       "ya"   -> "я", "yu" -> "ю", "yo" -> "ё", "ts" -> "ц", "a"  -> "а",
       "b"    -> "б", "v"  -> "в", "g"  -> "г", "d"  -> "д", "e"  -> "е",
-      "z"    -> "з", "i"  -> "и", "j"  -> "й", "k"  -> "к", "l"  -> "л",
+      "z"    -> "з", "i"  -> "и", "j"  -> "дж", "k"  -> "к", "l"  -> "л",
       "m"    -> "м", "n"  -> "н", "o"  -> "о", "p"  -> "п", "r"  -> "р",
-      "s"    -> "с", "t"  -> "т", "u"  -> "у", "f"  -> "ф", "y"  -> "ы"
+      "s"    -> "с", "t"  -> "т", "u"  -> "у", "f"  -> "ф", "y"  -> "й",
+      "x"    -> "кс", "th" -> "т", "w" -> "в", "h"  -> "х", "é"  -> "е",
+      "c"    -> "ц"
     )
     
     val femaleSeq = femaleNames.map(name => (name.toLowerCase, false)).toSeq
@@ -97,8 +107,9 @@ class PlanetEtlJob {
     val cyrillicKey = replacements.foldLeft(lower(col("parsed_name"))) {
       case (column, (from, to)) => regexp_replace(column, from, to)
     }
+    val firstWordCyrillic = regexp_extract(cyrillicKey, "^([^\\s,._—]+)", 1)  // remove double names
 
-    val preparedDf = df.withColumn("cyrillic_name", cyrillicKey)
+    val preparedDf = df.withColumn("cyrillic_name", firstWordCyrillic)
     val joinedDf = preparedDf.join(broadcast(dictionaryDf), col("cyrillic_name") === col("lookup_name"), "left")
 
     // 5. Build final column with clean fallbacks evaluated against the Cyrillic name
@@ -108,7 +119,7 @@ class PlanetEtlJob {
         .when(col("personal_status").isInCollection(femaleStatuses), false)
         .when(col("seeking").rlike("(?i)(женщин|девуш)"), true)
         .when(col("seeking").rlike("(?i)(мужчин|парн)"), false)
-        .when(col("cyrillic_name").rlike("(?i)[йнрмксвлй]$"), true)          // Match on Cyrillic endings
+        .when(col("cyrillic_name").rlike("(?i)[йнрмксвлй]$"), true)         // Match on Cyrillic endings
         .when(col("cyrillic_name").rlike("(?i)[ая]$"), false)               // Match on Cyrillic endings
         .otherwise(lit(null))
     ).drop("lookup_name", "is_male_dict", "cyrillic_name")
