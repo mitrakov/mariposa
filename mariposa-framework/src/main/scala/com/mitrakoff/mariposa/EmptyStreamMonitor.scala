16918afaa20a5b18e648b4646ec98cb6bbb7fc45
tommy <mitrakov-artem@yandex.ru>
2026-08-09 14:15:51 +0300

    EmptyStreamMonitor: fix NPE

--- mariposa-framework/src/main/scala/com/mitrakoff/mariposa/EmptyStreamMonitor.scala
+++ mariposa-framework/src/main/scala/com/mitrakoff/mariposa/EmptyStreamMonitor.scala
@@ -12,22 +12,25 @@ class EmptyStreamMonitor(minutes: Int) {
   def start(query: StreamingQuery): Unit = slave.scheduleAtFixedRate(() => run(query), 1, 1, TimeUnit.MINUTES)
   def stop(): Unit = slave.shutdown()
 
-  private def run(query: StreamingQuery): Unit = {
+  private def run(query: StreamingQuery): Unit = try {
     if (query.isActive) {
-      val rows = query.lastProgress.numInputRows
-      if (rows > 0) {
-        lastTimeDataSeen = System.currentTimeMillis()
-        logger.info(s"EmptyStreamMonitor: OK, I saw $rows rows")
-      } else {
-        val currentIdleDuration = System.currentTimeMillis() - lastTimeDataSeen
-        logger.info(s"EmptyStreamMonitor: streaming is empty for ${currentIdleDuration / 60000}/$minutes minutes.")
+      val progress = query.lastProgress
+      if (progress != null) {                  // may be null very first time
+        val rows = progress.numInputRows
+        if (rows > 0) {
+          lastTimeDataSeen = System.currentTimeMillis()
+          logger.info(s"OK, I saw $rows rows")
+        } else {
+          val currentIdleDuration = System.currentTimeMillis() - lastTimeDataSeen
+          logger.info(s"streaming is empty for ${currentIdleDuration / 60000}/$minutes minutes.")
 
-        if (currentIdleDuration >= minutes*60000) {
-          logger.warn(s"⚠️ EmptyStreamMonitor: streaming was empty for $minutes minutes. Stopping Spark...")
-          query.stop()
-          slave.shutdown()
+          if (currentIdleDuration >= minutes*60000) {
+            logger.warn(s"⚠️ streaming was empty for $minutes minutes. Stopping Spark...")
+            query.stop()
+            slave.shutdown()
+          }
         }
       }
     } else slave.shutdown()
-  }
+  } catch { case e: Exception => logger.error(s"ERROR ${e.getMessage}", e) }
 }
